478f8d6b232a79faf23d9d349cb46467400e23a0
gillesretiere <gilles.retiere@gmail.com>
2025-07-26 09:32:07 +0000

    Blog Stage One

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,15 +1,15 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'Blog Hammer & Marteau',
+  author: 'Gilles Retière',
+  headerTitle: 'Hammer & Marteau',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
-  socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
+  siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.svg`,
+  socialBanner: `${process.env.BASE_PATH || ''}/static/images/hmrt_logo_full.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'address@yoursite.com',
   github: 'https://github.com',
