4fd3950241e8f7eab4151153ad49ef9d640b0794
gillesretiere <gilles.retiere@gmail.com>
2025-09-08 18:03:49 +0000

    .

--- app/Main.tsx
+++ app/Main.tsx
@@ -11,7 +11,7 @@ export default function Home({ posts }) {
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="leading-9 font-thin tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100 text-3xl">
+          <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
             Expression Num&eacute;rique
           </h1>
           <p className="text-md text-primary-500 leading-7 font-thin dark:text-gray-400">
