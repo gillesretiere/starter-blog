602db08d0f7fa0110f4f3132ff3ba4c6cfbd07be
gillesretiere <gilles.retiere@gmail.com>
2025-09-09 06:21:53 +0000

    {/* prettier-ignore */}

--- app/Main.tsx
+++ app/Main.tsx
@@ -10,6 +10,7 @@ export default function Home({ posts }) {
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        {/* prettier-ignore */}
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl leading-9 font-thin tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
             Expression Num&eacute;rique
