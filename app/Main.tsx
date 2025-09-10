6880f5e784bf8dc3b7de3c01c81d98f55cc4a0d3
gillesretiere <gilles.retiere@gmail.com>
2025-09-10 15:55:59 +0000

    Fix

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,8 +3,6 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
-import Image from '@/components/Image'
-
 
 const MAX_DISPLAY = 5
 
