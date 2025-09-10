e249b7db1c00e2259e00d583093b32ce9190179b
gillesretiere <gilles.retiere@gmail.com>
2025-09-10 15:52:33 +0000

    Banner image

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,6 +3,8 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
+import Image from '@/components/Image'
+
 
 const MAX_DISPLAY = 5
 
@@ -12,9 +14,9 @@ export default function Home({ posts }) {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         {/* prettier-ignore */}
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl leading-9 font-thin tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
-            Expression Num&eacute;rique
-          </h1>
+          <img
+            src="/static/images/freepik__expand__15158_pano.png"
+          />
           <p className="text-md text-primary-500 leading-7 font-thin dark:text-gray-400">
             {siteMetadata.description}
           </p>
