e5c37cfbae4babcb22ddd7607842a02e4e9b815a
gillesretiere <gilles.retiere@gmail.com>
2025-11-17 10:15:04 +0000

    fix PageTitle

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function PageTitle({ children }: Props) {
   return (
-    <h1 className="text-3xl leading-9 font-medium tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14 dark:text-gray-100 text-left">
+    <h1 className="text-left text-3xl leading-9 font-medium tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14 dark:text-gray-100 ">
       {children}
     </h1>
   )
